047019df39590d60efac9bfb00b1ec5920b1d14c
Sean <shonkelli@gmail.com>
2019-05-21 19:06:25 -0400

    added ability to delete tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/tasks/batch_create', to: 'tasks#batch_create'
-  resources :tasks, only: [:create, :index, :update]
+  resources :tasks, only: [:create, :index, :update, :destroy]
 end
