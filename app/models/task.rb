54ecc3c4a9565775e29ffb878966526fd7992793
Sean <shonkelli@gmail.com>
2019-05-23 22:20:26 -0400

    removed console statements

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ApplicationRecord
 
   def self.update_cat_indexes(category_data)
-
     # change category of moved task
     updated_task = category_data["updatedTask"]
     Task.update(updated_task[:id], category: updated_task[:category])
@@ -11,14 +10,8 @@ class Task < ApplicationRecord
     updated_categories.each_pair do |key, tasks|
       task_hash = {}
       tasks.each_with_index do |task, index|
-        puts ''
-        puts ''
-        puts ''
-        puts ''
-        puts "Converting task #{task[:content]} index #{task[:category_index]} to #{index}"
         task_hash[task[:id]] = {category_index: index}
       end
-
       Task.update(task_hash.keys, task_hash.values)
     end
   end
